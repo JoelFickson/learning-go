2893bcc27a9dcf151864cec0ece1e4e3623a1905
Joel Fickson <joel.fickson@scalingfunds.com>
2022-06-08 21:27:27 +0200

    init: first time trials

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var greetingTheWorld = "THIS IS MY FIRST TIME USING GO!"
+
+	fmt.Println("################################################################")
+	fmt.Println(greetingTheWorld)
+}
