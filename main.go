b5632215c84a66dbba27af6a7ad196fde5019071
Joel Fickson <joel.fickson@scalingfunds.com>
2022-06-08 21:56:06 +0200

    added array data

--- main.go
+++ main.go
@@ -7,4 +7,14 @@ func main() {
 
 	fmt.Println("################################################################")
 	fmt.Println(greetingTheWorld)
+
+	var MalawiDiscricts [3]string
+
+	// Elements are assigned using index
+	MalawiDiscricts[0] = "Lilongwe"
+	MalawiDiscricts[1] = "Blantrye"
+	MalawiDiscricts[2] = "Zomba"
+
+	fmt.Print(MalawiDiscricts[0])
+
 }
